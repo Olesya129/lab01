02e0832be04128ce42154173e66777431b4bac27
Olesya129 <olesya.dulesova@mail.ru>
2024-10-12 12:59:31 +0500

    Алгоритм Евклида

---
+++ main.py
@@ -0,0 +1,10 @@
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+
+result = gcd(num1, num2)
+print(f"Наибольший общий делитель (НОД) чисел {num1} и {num2} = {result}")
